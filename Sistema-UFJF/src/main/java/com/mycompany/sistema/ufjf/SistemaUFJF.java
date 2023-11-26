4696d979110d1b0bcde9c008bc85581c4576a40f
Rafael Silva <117786730+RafaSilva13@users.noreply.github.com>
2023-11-26 16:16:06 -0300

    Add files via upload
    Adição de arquivos

---
+++ Sistema-UFJF/src/main/java/com/mycompany/sistema/ufjf/SistemaUFJF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema.ufjf;
+
+/**
+ *
+ * @author rafael.pereira
+ */
+public class SistemaUFJF {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
