d811e21c1717053b9bdcdbddfa7d2a62b6138b30
Caio Braida <caiobraida.p@gmail.com>
2023-12-03 11:18:00 -0300

    desenvolvimento classe data e classe exeção de data

--- Sistema-UFJF/src/main/java/com/mycompany/sistema/ufjf/SistemaUFJF.java
+++ Sistema-UFJF/src/main/java/com/mycompany/sistema/ufjf/SistemaUFJF.java
@@ -4,9 +4,14 @@ package com.mycompany.sistema.ufjf;
  *
  * @author rafael.pereira
  */
+import com.mycompany.sistema.ufjf.exeptions.DataExeption;
+import com.mycompany.sistema.ufjf.model.Data;
+
 public class SistemaUFJF {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DataExeption {
         System.out.println("Hello World!");
+        Data data = Data.parser("21/01/1111");
+        System.out.println(data.toString());
     }
 }
