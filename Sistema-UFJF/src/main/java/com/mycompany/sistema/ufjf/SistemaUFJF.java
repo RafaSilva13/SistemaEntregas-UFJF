1681e0e541ca0c0825a77f9691802a1db1611537
Caio Braida <caiobraida.p@gmail.com>
2023-12-03 16:01:13 -0300

    test push

--- Sistema-UFJF/src/main/java/com/mycompany/sistema/ufjf/SistemaUFJF.java
+++ Sistema-UFJF/src/main/java/com/mycompany/sistema/ufjf/SistemaUFJF.java
@@ -7,6 +7,6 @@ package com.mycompany.sistema.ufjf;
 public class SistemaUFJF {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! teste -");
     }
 }
