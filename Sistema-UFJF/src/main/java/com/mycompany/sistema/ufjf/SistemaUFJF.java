d33678db2ffbfac50ac7563e6e7956106cf57009
Caio Braida <caiobraida.p@gmail.com>
2023-12-03 13:47:07 -0300

    cria classe email e execeção de email

--- Sistema-UFJF/src/main/java/com/mycompany/sistema/ufjf/SistemaUFJF.java
+++ Sistema-UFJF/src/main/java/com/mycompany/sistema/ufjf/SistemaUFJF.java
@@ -9,12 +9,16 @@ import com.mycompany.sistema.ufjf.model.*;
 
 public class SistemaUFJF {
 
-    public static void main(String[] args) throws DataExeption, CpfExeption {
+    public static void main(String[] args) throws DataExeption, CpfExeption, EmailException {
         System.out.println("Hello World!");
         Data data = Data.parser("21/01/1111");
         System.out.println(data.toString());
         
         Cpf cpf = Cpf.parser("112.333112-11");
         System.out.println(cpf.toString());
+        
+        Email email = new Email("caiogmail.com");
+        System.out.println(email.toString());
+        
     }
 }
